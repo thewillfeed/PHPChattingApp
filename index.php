7d0d6ba2d2123e956e170e8c3d94be0f9ce50d75
TheWill <willimossop@gmail.com>
2022-09-06 14:12:50 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="ru">
+<html>
+    <head>
+        <!-- charset and viewport stuff -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+
+        <!-- Title and author meta -->
+        <title>A Basic HTML5 Template</title>
+        <meta name="description" content="A simple HTML5 Template for new projects.">
+        <meta name="author" content="SitePoint">
+
+        <!-- Embed stuff -->
+        <meta property="og:title" content="A Basic HTML5 Template">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://www.sitepoint.com/a-basic-html5-template/">
+        <meta property="og:description" content="A simple HTML5 Template for new projects.">
+        <meta property="og:image" content="image.png">
+
+        <!-- favicon icon links -->
+        <link rel="icon" href="/favicon.ico">
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    </head>
+    <body>
+        <header>
+
+        </header>
+        <main>
+
+        </main>
+        <footer>
+
+        </footer>
+    <!-- <script src="js/scripts.js"></script> -->
+    </body>
+</html>
